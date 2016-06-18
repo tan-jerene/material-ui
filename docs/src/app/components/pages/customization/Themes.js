5304fc833e6cf680f7bdc9cbba4a8228c392a79c
Jerene Ricketts <jerene.ricketts@gmail.com>
2016-06-18 00:39:35 -0500

    fixed?

--- docs/src/app/components/pages/customization/Themes.js
+++ docs/src/app/components/pages/customization/Themes.js
@@ -17,6 +17,7 @@ import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {cyan500, red500} from 'material-ui/styles/colors';
+import {darken, fade, emphasize, lighten} from '../utils/colorManipulator';
 
 import {
   AppBar,
